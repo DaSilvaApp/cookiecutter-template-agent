8c030984faf3e2b0e22575689f49c4c5316b3ae0
DaSilva <leonel.baptista@mrig.de>
2026-03-09 10:23:59 +0000

    Changed the logger file

--- {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/core/logger.py
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/core/logger.py
@@ -4,21 +4,20 @@ from logging.handlers import RotatingFileHandler
 
 from rich.logging import RichHandler
 
-# Need to get the config to the logs from the config file
-
 LOG_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs")
 os.makedirs(LOG_DIR, exist_ok=True)
-
 LOG_FILE = os.path.join(LOG_DIR, "app.log")
 
 logger = logging.getLogger("app_logger")
 logger.setLevel(logging.DEBUG)
+logger.propagate = False
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-handler = RotatingFileHandler(LOG_FILE, maxBytes=5_000_000, backupCount=3)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+file_handler = RotatingFileHandler(LOG_FILE, maxBytes=5_000_000, backupCount=3)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
-console_handler = RichHandler()
+console_handler = RichHandler(rich_tracebacks=True, show_path=False)
+console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
